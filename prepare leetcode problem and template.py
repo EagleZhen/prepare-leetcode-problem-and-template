bf614f9062e4397f6dc22f6130c35228ed624729
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-09-08 16:14:59 +0800

    scrape leetcode template c++ codes

--- prepare leetcode problem and template.py
+++ prepare leetcode problem and template.py
@@ -4,44 +4,64 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 from markdownify import markdownify as md
 import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def setup_selenium() -> WebDriver:
     chrome_options = Options()
     # chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(10)
     return driver
 
 
 def get_problem_title_in_plaintext(driver: WebDriver) -> str:
-    title_element = driver.find_element(
-        By.CSS_SELECTOR, "a.no-underline.truncate.cursor-text"
+    title_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "a.no-underline.truncate.cursor-text"))
     )
     text_content = title_element.text
     return text_content
 
 
 def get_problem_description_in_markdown(driver: WebDriver) -> str:
-    problem_description_element = driver.find_element(By.CLASS_NAME, "elfjS")
+    problem_description_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "elfjS"))
+    )
     html_content = problem_description_element.get_attribute("outerHTML")
     markdown_content = md(html_content)
     return markdown_content
 
+def get_template_code(driver: WebDriver) -> str:
+    # Locate all the lines of the code by class name "view-line"
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "view-line"))
+    )
+    code_lines = driver.find_elements(By.CLASS_NAME, "view-line")
 
-def scrape_leetcode_problem(driver: WebDriver, url: str) -> dict:
+    # Extract and concatenate all the text
+    cpp_code = ""
+    for line in code_lines:
+        span = line.find_element(By.TAG_NAME, "span") # Only need the outermost span that contains all the words in the line
+        cpp_code += span.text
+        cpp_code += "\n"
+
+    return cpp_code
+
+def scrape_leetcode(driver: WebDriver, url: str) -> dict:
     # Open the webpage
     driver.get(url)
     driver.implicitly_wait(10)
 
     problem_title = get_problem_title_in_plaintext(driver)
-    # print("Problem Title:", problem_title)
-
     problem_description_in_markdown = get_problem_description_in_markdown(driver)
+    template_code = get_template_code(driver)
 
     return {
         "url": url,
         "title": problem_title,
         "description": problem_description_in_markdown,
+        "template": template_code,
     }
 
 
@@ -81,15 +101,19 @@ def construct_readme_file(data: dict, directory: str) -> None:
     with open(os.path.join(directory, "README.md"), "w", encoding="utf-8") as f:
         f.write(readme_content)
 
+def construct_template_file(data: dict, directory: str) -> None:
+    with open(os.path.join(directory, f"{data["title"]}.cpp"), "w", encoding="utf-8") as f:
+        f.write(data["template"])
 
 if __name__ == "__main__":
     driver = setup_selenium()
 
     url = "https://leetcode.com/problems/robot-collisions/description/"
-    data = scrape_leetcode_problem(driver, url)
+    data = scrape_leetcode(driver, url)
 
     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output", data["title"])
     create_directory(directory)
     construct_readme_file(data, directory)
+    construct_template_file(data, directory)
 
     driver.quit()
