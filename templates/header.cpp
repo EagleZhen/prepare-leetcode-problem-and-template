4e3021f2b7452697dd0cd70bb91fb8c183d72a33
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-10-11 01:16:21 +0800

    Update header.cpp

--- templates/header.cpp
+++ templates/header.cpp
@@ -1,9 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
+#define debug if (0)
 
-const int MAXN = 1e5+5;
+const int MAXN = 1e5 + 5;
+const int MAX_VAL = 2e9 + 5;
+const int MIN_VAL = -(2e9 + 5);
 
 /*
 
