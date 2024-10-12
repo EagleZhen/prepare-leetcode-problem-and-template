6d0f8d5d9d99249edc5cd4c016a2ccee1c21ddcb
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-10-12 15:27:42 +0800

    minor update on template files

--- templates/footer.cpp
+++ templates/footer.cpp
@@ -1,14 +1,18 @@
+/*
+
+*/
+
 int main(){
     Solution solution;
 
-    vector<int> v1, v2;
+    vector<int> v1, output;
     int i1, i2;
 
     v1 = {};
     i1 = 0;
     i2 = 0;
-    v2 = solution.f(v1, v2);
-    assert(v2 == vector<int>({1,2,3}));
+    output = solution.f(v1);
+    assert(output == vector<int>({1,2,3}));
 
     return 0;
 }
--- templates/header.cpp
+++ templates/header.cpp
@@ -7,7 +7,3 @@ using namespace std;
 const int MAXN = 1e5 + 5;
 const int MAX_VAL = 2e9 + 5;
 const int MIN_VAL = -(2e9 + 5);
-
-/*
-
-*/
